589203207372ea3c5e217f3ae2d85831e65bd1b2
IanR01 <ian.r.89@gmail.com>
2024-05-01 17:33:50 +0200

    add logging in import service

--- services/openstad/importService.js
+++ services/openstad/importService.js
@@ -22,8 +22,11 @@ exports.handleImportFile = async (newSite, file, fileUrl) => {
     }
   }
 
+  
   const filename = newSite.getTmpDir() + '/' + file.originalname;
-
+  
+  console.log(`===> The filename is: ${filename}`)
+  console.log(`===> Importing a file. Trying to crate e directory at: ${newSite.getTmpDir()}`)
   // createImportDir
   await fs.mkdir(newSite.getTmpDir());
 
