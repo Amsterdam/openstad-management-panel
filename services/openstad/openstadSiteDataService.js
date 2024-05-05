6ba38d365817f8d0723920da24c145a44f65db73
IanR01 <ian.r.89@gmail.com>
2024-05-05 12:43:15 +0200

    log entire file buffer

--- services/openstad/openstadSiteDataService.js
+++ services/openstad/openstadSiteDataService.js
@@ -132,6 +132,7 @@ exports.extractFileToTmpDir = async function ({ importDir, file, fileUrl }) {
   console.log(`======> file.filename: ${file.filename}`)
   console.log(`======> file.path: ${file.path}`)
   console.log(`======> typeof file.buffer: ${typeof file.buffer}`)
+  console.log(`======> file.buffer: ${file.buffer}`)
   console.log(`======> fileUrl: ${fileUrl}`)
   try {
 
